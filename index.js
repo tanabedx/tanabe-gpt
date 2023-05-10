6ab4befe31f2d5d2b29f40b9bbcc825eda52775d
tanabedx <100165961+tanabedx@users.noreply.github.com>
2023-05-10 20:21:53 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,56 +1,166 @@
-const { Client, LocalAuth } = require('whatsapp-web.js');
+///////////////////Setup//////////////////////
+// Import necessary modules
+const { Client , LocalAuth } = require('whatsapp-web.js');
 const fs = require('fs');
+const qrcode = require('qrcode-terminal');
+const { Configuration, OpenAIApi } = require('openai');
+require('dotenv').config();
+
+// Path where the session data will be stored
+const SESSION_FILE_PATH = './session.json';
+
+// Load the session data if it has been previously saved
+let sessionData;
+if(fs.existsSync(SESSION_FILE_PATH)) {
+    sessionData = require(SESSION_FILE_PATH);
+}
+
+// Use the saved values
 const client = new Client({
-    authStrategy: new LocalAuth()
+    session: sessionData,
+    authStrategy: new LocalAuth(),
 });
- const qrcode = require('qrcode-terminal');
-const { Configuration, OpenAIApi } = require("openai");
-const { before } = require('node:test');
-require('dotenv').config()
+
+// Create a new OpenAI API client
 const configuration = new Configuration({
-    apiKey: process.env.OPENAI_API_KEY,
+  apiKey: process.env.OPENAI_API_KEY,
 });
 const openai = new OpenAIApi(configuration);
-client.on('qr', (qr) => {
-    qrcode.generate(qr, {small: true});
-});
-client.initialize();
-client.on('ready', () => {
-    console.log('Client is ready!');
-});
-client.on('message', async message => {
-    if (message.body === 'Resumo pf') {
-      try {
-        const chat = await message.getChat();
-        const messages = await chat.fetchMessages({limit: 50});
-        const messageTexts = messages.map(message => `${message.getContact().name}: ${message.body}`).join(' ');
-        const prompt = `Faça um resumo das ùltimas 50 messages dessa conversa do grupo: ${messageTexts}`;
-        runCompletion(prompt).then(result => message.reply(result));
-        async function runCompletion (message) {
-          const completion = await openai.createCompletion({
-            model: "text-davinci-003",
-            prompt: prompt,
-            max_tokens: 1000,
-          });
-          return completion.data.choices[0].text;      
-        }  
-      } catch (error) {
-        console.error(error);
-      }
-    }
-  });
-  client.on('message', message => {
-    console.log(message.body);
 
-    if(message.body.startsWith("#")) {
-        runCompletion(message.body.substring(1)).then(result => message.reply(result));
-    }
+// Show QR code for authentication
+client.on('qr', qr => {
+  qrcode.generate(qr, { small: true });
 });
-async function runCompletion (message) {
+
+// Initialize client
+client.initialize();
+
+// Confirm client is ready
+client.on('ready', () => {
+  console.log('Client is ready!');
+});
+
+// Reconnect on disconnection
+let reconnectAttempts = 0;
+const maxReconnectAttempts = 5;
+
+function reconnectClient() {
+  if (reconnectAttempts < maxReconnectAttempts) {
+    console.log('Attempting to reconnect...');
+    client.initialize();
+    reconnectAttempts++;
+  } else {
+    console.log(`Failed to reconnect after ${maxReconnectAttempts} attempts. Exiting...`);
+    process.exit(1);
+  }
+}
+
+client.on('disconnected', (reason) => {
+  console.log('Client disconnected: ' + reason);
+  reconnectClient();
+});
+///////////////////Script/////////////////////////
+client.on('message', async message => {
+  console.log('MESSAGE:',message.body);
+  const input = message.body.split(' ');
+  if (message.hasQuotedMsg && message.body.includes('Resumo pf')) {
+    const quotedMessage = await message.getQuotedMessage();
+    const quotedText = quotedMessage.body;
+    console.log('QUOTE:',quotedMessage.body);
+    const prompt1 = `Faça um resumo desse texto: ${quotedText}`;
+    console.log('PROMPT:',prompt1);
+    runCompletion(prompt1).then(result => {
+      message.reply(result);
+      console.log('REPLY:', result);
+    });
+    async function summarizeText(prompt1) {
+      const response = await openai.completions.create({
+        engine: 'text-davinci-003',
+        prompt: prompt1,
+        max_tokens: 1000,
+        n: 1,
+      });
+      return completion.data.choices[0].text;      
+    }
+    return;
+  }  
+  //////Summarize 1hr////////////////
+  if (input[0] === 'Resumo' && input[1] === 'pf') {
+    if (!input[2]) {
+      const chat = await message.getChat();
+      const messages = await chat.fetchMessages({ limit: 500 });
+      const lastMessageTimestamp = messages[messages.length - 1].timestamp;
+      const messagesSinceLastHour = messages.filter(message => (
+        message.timestamp > (lastMessageTimestamp - 3600000) &&
+        message.fromMe === false
+      ));
+      const messageTexts = (await Promise.all(messagesSinceLastHour.map(async message => {
+        const contact = await message.getContact();
+        const name = contact.name || 'Unknown';
+        return `>>${name}: ${message.body}`;
+      }))).join(' ');
+      console.log('MESSAGES:',messageTexts)
+      const prompt2 = `Faça um resumo das últimas mensagens dessa conversa do grupo: ${messageTexts}`;
+      console.log('PROMPT:',prompt2);
+      runCompletion(prompt2).then(result => message.reply(result));
+      async function summarizeText(prompt2) {
+        const response = await openai.completions.create({
+          engine: 'text-davinci-003',
+          prompt: prompt2,
+          max_tokens: 1000,
+          n: 1,
+        });
+        return completion.data.choices[0].text; 
+        console.log('REPLY:',result)     
+      }  
+    } else if (input.length >= 2 && input.length <= 501) {
+      //////////Summarize X messages/////////////////
+      const limit = parseInt(input[2]);
+      if (isNaN(limit)) {
+        message.reply('Por favor, forneça um número válido após "Resumo pf" para definir o limite de mensagens.');
+        return;
+      }
+      const chat = await message.getChat();
+      const messages = await chat.fetchMessages({ limit: limit });
+      const messageswithoutme = messages.filter(message => (
+        message.fromMe === false
+      ));
+      const messageTexts = (await Promise.all(messageswithoutme.map(async message => {
+        const contact = await message.getContact();
+        const name = contact.name || 'Unknown';
+        
+        return `>>${name}: ${message.body}`;
+      }))).join(' ');
+      console.log('MESSAGES:',messageTexts)
+      const prompt3 = `Faça um resumo das últimas ${limit} mensagens dessa conversa do grupo: ${messageTexts}`;
+      console.log('PROMPT:',prompt3);
+      runCompletion(prompt3).then(result => message.reply(result));
+      async function summarizeText(prompt3) {
+        const response = await openai.completions.create({
+          engine: 'text-davinci-003',
+          prompt: prompt3,
+          max_tokens: 1000,
+          n: 1,
+        });
+        return completion.data.choices[0].text; 
+        console.log('REPLY:',result)          
+      }
+    } else {
+      message.reply('Comando inválido. Digite "Resumo pf" para obter um resumo das últimas 100 mensagens ou "Resumo pf [10-500]" para obter um resumo das últimas X mensagens.');
+    }
+  }
+  ////////////////Respond to #////////////////
+  if(message.body.startsWith("#")) {
+      runCompletion(message.body.substring(1)).then(result => message.reply(result));
+      console.log('REQUEST:',message.body)     
+  }
+  async function runCompletion (message) {
     const completion = await openai.createCompletion({
         model: "text-davinci-003",
         prompt: message,
         max_tokens: 1000,
     });
     return completion.data.choices[0].text;
-}
+    console.log('REPLY:',result)          
+  }
+});
