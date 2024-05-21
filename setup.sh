db087dbef53942ab4237ac24eb4beac9c18f79b9
tanabedx <100165961+tanabedx@users.noreply.github.com>
2024-05-21 14:12:07 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -22,8 +22,8 @@ npm install --no-audit
 npm update -g --no-audit
 
 sudo apt-get install -y gconf-service libasound2 libatk1.0-0 libc6 libcairo2 libcups2 libdbus-1-3 libexpat1 libfontconfig1 libgcc1 libgconf-2-4 libgdk-pixbuf2.0-0 libglib2.0-0 libgtk-3-0 libnspr4 libpango-1.0-0 libpangocairo-1.0-0 libstdc++6 libx11-6 libx11-xcb1 libxcb1 libxcomposite1 libxcursor1 libxdamage1 libxext6 libxfixes3 libxi6 libxrandr2 libxrender1 libxss1 libxtst6 ca-certificates fonts-liberation libappindicator1 libnss3 lsb-release xdg-utils wget libgbm-dev
-
-mkdir stickers
-
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_x86_64.rpm
+sudo yum localinstall google-chrome-stable_current_x86_64.rpm
+npm install github:pedroslopez/whatsapp-web.js#webpack-exodus --no-audit
 # Provide feedback to the user
 echo "Setup complete. index.js and .env files created if they didn't exist, npm packages installed, global npm packages updated, and .env file populated with the OpenAI API key."
